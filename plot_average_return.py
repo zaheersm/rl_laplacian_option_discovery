3a19000cb240d33458eb3cd2afd260c90165b07b
Sungsu <ss.sungsulim@gmail.com>
2017-11-25 13:53:22 -0700

    modify to plot all options

--- plot_average_return.py
+++ plot_average_return.py
@@ -12,8 +12,12 @@ if __name__ == "__main__":
    graph_agent_4, = plt.plot(x_legend, Results[4][:], label="4 options")
    graph_agent_8, = plt.plot(x_legend, Results[8][:], label="8 options")
    graph_agent_64, = plt.plot(x_legend, Results[64][:], label="64 options")
+   graph_agent_128, = plt.plot(x_legend, Results[128][:], label="128 options")
+   graph_agent_200, = plt.plot(x_legend, Results[200][:], label="200 options")
 
-   plt.legend(handles=[graph_agent_0, graph_agent_2, graph_agent_4, graph_agent_8, graph_agent_64])
+
+
+   plt.legend(handles=[graph_agent_0, graph_agent_2, graph_agent_4, graph_agent_8, graph_agent_64, graph_agent_128, graph_agent_200])
    plt.xlabel('Episodes')
    plt.ylabel('Average return')
    plt.show()
